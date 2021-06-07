3bd8421193931d004cdd163c148faa235ed0eac7
zodern <zodern@users.noreply.github.com>
2021-06-07 12:35:35 -0500

    Fix tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 Tinytest.add(
   'v8profiler module',
   function function_name (test) {
-    var v8profiler = KadiraBinaryDeps.require('v8-profiler');
+    var v8profiler = KadiraBinaryDeps.require('v8-profiler-next');
     test.equal(typeof v8profiler.takeSnapshot, 'function');
   }
 );
