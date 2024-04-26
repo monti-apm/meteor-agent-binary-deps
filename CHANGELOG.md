f9a1a3861906cf218e21fe4d4b0652777293d9f7
zodern <zodern@users.noreply.github.com>
2024-04-25 22:12:39 -0500

    v3.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## v3.0.0
+
+* Drop support for Meteor 1.4 - 1.8.2
+* Update v8-profiler-next
+* Add support for Meteor 3
+* Rename KadiraBinaryDeps to MontiBinaryDeps
+
 ## v2.1.1
 
 * Update v8-profiler-next to fix memory leak
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Binary Dependencies for montiapm:agent",
-  "version": "2.1.1",
+  "version": "3.0.0",
   "git": "https://github.com/monti-apm/meteor-agent-binary-deps.git",
   "name": "montiapm:agent-binary-deps"
 });
